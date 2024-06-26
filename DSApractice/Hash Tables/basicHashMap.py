5d6935387253d1cc3a85ea58d631b9e6ee8e1d92
Aryaman Gautam <aryamangautamg@gmail.com>
2024-06-25 23:26:07 -0700

    basic hash map

--- DSApractice/Hash Tables/basicHashMap.py
+++ DSApractice/Hash Tables/basicHashMap.py
@@ -26,5 +26,37 @@ class SimpleHashMap:
         return None
     def remove(self, key):
         index = self.hash_function(key)
-        #finish later
+        bucket = self.buckets[index]
+        for i, (k, _) in enumerate(bucket):
+            if k == key:
+                del bucket[i]
+                return
+    def print_map(self):
+        print("Hash Map:")
+        for index, bucket in enumerate(self.buckets):
+            print(f"Bucket {index}: {bucket}")
+    
+# Creating the Hash Map from the simulation
+hash_map = SimpleHashMap(size=10)
+
+# Adding some entries
+hash_map.put("123-4567", "Charlotte")
+hash_map.put("123-4568", "Thomas")
+hash_map.put("123-4569", "Jens")
+hash_map.put("123-4570", "Peter")
+hash_map.put("123-4571", "Lisa")
+hash_map.put("123-4672", "Adele")
+hash_map.put("123-4573", "Michaela")
+hash_map.put("123-6574", "Bob")
+
+hash_map.print_map()
+
+# Demonstrating retrieval
+print("\nName associated with '123-4570':", hash_map.get("123-4570"))
+
+print("Updating the name for '123-4570' to 'James'")
+hash_map.put("123-4570","James")
+
+# Checking if Peter is still there
+print("Name associated with '123-4570':", hash_map.get("123-4570"))
     
